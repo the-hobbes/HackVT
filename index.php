0358b1d058bb2e72ee413feb79266798bf4f44d0
Dillan J. Wilding <dillanjwilding@gmail.com>
2012-10-19 18:26:07 -0400

    Adding more meta data and optimizing existing setup.

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 	<head>
 		<title>HackVT</title>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<!-- Mobile viewport optimized: h5bp.com/viewport -->
+		<meta name="viewport" content="width=device-width">
         <meta name="author" content="Dillan, Phelan, Garth, Scott, Ethan." />
         <meta name="description" content="" />
         <meta name="keywords" content="HackVT, Hackathon, etc." />
 
         <link rel="stylesheet" href="style.css" type="text/css" media="screen">
-        <script src="script.js" type="text/javascript"></script>   
 	</head>
 	<body>
 		<header>
@@ -16,5 +18,9 @@
 		</header>
 		<div id="content"></div>
 		<footer></footer>
+		<!-- JavaScript at the bottom for fast page loading -->
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+			<script src="script.js" type="text/javascript"></script>   
+		<!-- end scripts -->
 	</body>
 </html>
