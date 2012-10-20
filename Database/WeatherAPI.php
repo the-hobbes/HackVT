17d7bf2034108900d3cb13f86b599558513b6d8b
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-10-19 19:28:39 -0700

    got it to give me data now I just have to look through it

--- Database/WeatherAPI.php
+++ Database/WeatherAPI.php
@@ -15,25 +15,40 @@ class weatherAPIInterface{
 
 	public function __construct(){
 		$this->getCities();
+		$this->getWeather();
 	}
 
-	public function getCities(){
-		//Start getting ready to ask for data (Refactor this to go through dates)
-		$ch = curl_init('http://api.wunderground.com/api/'.$this->key.'/history_20100101/q/'.$this->state.'.json');
+	public function curlOn($url){
+		$ch = curl_init($url);
 		//Set options, I want to fail silently if I get a 404 page because I can't parse that
 		//             I want to follow anything I need to, and get the data as a string
 		curl_setopt($ch,CURLOPT_FAILONERROR,true);
 		curl_setopt($ch,CURLOPT_AUTOREFERER,true);
-//		curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
 		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-		$output = json_decode(curl_exec($ch));
+		$out = curl_exec($ch);
 		//free up resources
 		curl_close($ch);
+		return $out;
+	}
 
+	public function getCities(){
+		//Start getting ready to ask for data (Refactor this to go through dates)
+		$output = json_decode($this->curlOn('http://api.wunderground.com/api/'.$this->key.'/history_20100101/q/'.$this->state.'.json'));
+		
+		//Grab cities out of the data
 		$this->cities = array();
-
 		foreach ($output->response->results as $key) {
 			$this->cities[] = ($key->city);
+		}
+	}
+
+	public function getWeather(){
+		//Initilization check.
+		if(is_null($this->cities)){return;}
+		foreach ($this->cities as $city) {
+			$data = $this->curlOn('http://api.wunderground.com/api/'.$this->key.'/history_20100101/q/'.$this->state.'/'.$city.'.json');
+			print_r($data);
+			echo '<br />';
 		}
 	}
 
