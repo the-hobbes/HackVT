d18b1b4029ecb6c8621f64acbd776948893ef3d1
the-hobbes <phelan.vendeville@gmail.com>
2012-10-19 21:54:30 -0400

    added navigation and modularized scripts

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>HackVT: Collateral Damage - Recipe Farm</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+		<!-- Mobile viewport optimized: h5bp.com/viewport -->
+		<meta name="viewport" content="width=device-width">
+        <meta name="author" content="Dillan, Phelan, Garth, Scott, Ethan." />
+        <meta name="description" content="" />
+        <meta name="keywords" content="HackVT, Hackathon, Collateral Damage, Recipe Farm, etc." />
+
+        <link rel="stylesheet" href="style.css" type="text/css" media="screen">
+	</head>
+	<body>
+		<header>
+			<nav>
+				<ul>
+					<li><a href="index.php">Home</a></li>
+					<li><a href="link2.php">Link 2</a></li>
+					<li>
+						<a href="dropdown1.php">Drop Down 1</a>
+						<ul class="submenu">
+							<li><a href="link4.php">Link 4</a></li>
+							<li><a href="link5.php">Link 5</a></li>
+						</ul>
+					</li>
+					<li>
+						<a href="dropdown2.php">Drop Down 2</a>
+						<ul class="submenu">
+							<li><a href="link6.php">Link 6</a></li>
+						</ul>
+					</li>
+					<li><a href="link3.php">Link 3</a></li>
+				</ul>
+			</nav>
+		</header>
+		<div id="content"></div>
+		<footer></footer>
+		<!-- JavaScript at the bottom for fast page loading -->
+			<script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+			<script src="script.js" type="text/javascript"></script>   
+		<!-- end scripts -->
+	</body>
+</html>
