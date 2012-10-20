996328d8241116f9642d0d4dd2f41901bbbb5cef
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-10-19 19:07:44 -0700

    started setting up json curl on state stations

--- Database/WeatherAPI.php
+++ Database/WeatherAPI.php
@@ -3,10 +3,31 @@
 //2012 October 19
 //Hack VT Database Weather Aggregation.
 /*
-
-
+Weather API a8be19e4c204a4b0
+This query will get us all of VT
+http://api.wunderground.com/api/a8be19e4c204a4b0/history_20100101/q/VT.json
 */
 
+class weatherAPIInterface{
+	private $key = 'a8be19e4c204a4b0';
 
+	public function __construct(){}
+
+	public getStateWide(){
+		//Start getting ready to ask for data (Refactor this to go through dates)
+		$ch = curl_init('http://api.wunderground.com/api/'.$this->key.'/history_20100101/q/VT.json');
+		//Set options, I want to fail silently if I get a 404 page because I can't parse that
+		//             I want to follow anything I need to, and get the data as a string
+		curl_setopt($ch,CURLOPT_FAILONERROR,true);
+		curl_setopt($ch,CURLOPT_AUTOREFERER,true);
+//		curl_setopt($ch, CURLOPT_BINARYTRANSFER, true);
+		curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+		$output = json_decode(curl_exec($ch));
+		//free up resources
+		curl_close($ch);
+
+	}
+
+}
 
 ?>
