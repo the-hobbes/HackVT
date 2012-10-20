4570e57cb43825e2912c6b72d741341b79a710b6
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-10-19 23:37:40 -0700

    have code to snag database of weather

--- Database/WeatherAPI.php
+++ Database/WeatherAPI.php
@@ -15,6 +15,7 @@ class weatherAPIInterface{
 	private $key = 'a8be19e4c204a4b0';
 	private $state = 'VT';
 	private $cities = null;
+	public $apiRequests = 0;
 
 	public function __construct(){
 		$this->db = new mySQLDB();
@@ -32,6 +33,7 @@ class weatherAPIInterface{
 		$out = curl_exec($ch);
 		//free up resources
 		curl_close($ch);
+		$this->apiRequests = $this->apiRequests + 1;
 		return $out;
 	}
 
@@ -53,12 +55,11 @@ class weatherAPIInterface{
 		$weather['date'] = "2012-01-01"; 
 		if(is_null($this->cities)){return;}
 		for($i =0; $i < 71; $i++){
-
 			foreach ($this->cities as $city) {
 				//increment the date starting at the start date
 				set_time_limit(60);
 				//Will be date to query on later on
-				$data = json_decode($this->curlOn('http://api.wunderground.com/api/'.$this->key.'/history_20100101/q/'.$this->state.'/'.urlencode($city).'.json'));
+				$data = json_decode($this->curlOn('http://api.wunderground.com/api/'.$this->key.'/history_'.implode(explode('-',$weather['date'])).'/q/'.$this->state.'/'.urlencode($city).'.json'));
 				$dayData = $data->history->dailysummary; $dayData = $dayData[0];
 				//Get the important things out of the query:
 				$weather['maxHum'] = $dayData->maxhumidity;
@@ -71,10 +72,15 @@ class weatherAPIInterface{
 				$weather['minTemp']= $dayData->mintempi;
 				$weather['avgTemp']= $dayData->meantempi;
 				
-				print_r($weather);
-
 				$this->db->inputWeather($weather);
-				echo '<br /><br />';
+
+				if($this->apiRequests % 10){
+					sleep(10);
+					if($this->apiRequests >= 500){
+						die('CANT ASK FOR MORE CAPTAIN');
+					}
+				}
+
 			}
 			$weather['date'] = date("Y-m-d",strtotime("+1 day", strtotime($weather['date'])));
 			
