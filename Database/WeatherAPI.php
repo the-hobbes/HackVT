489cb42a540fd35fce5f20766f51c9e8bdd88708
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2012-10-19 21:28:05 -0700

    finished up the insert, now I need to make the date work

--- Database/WeatherAPI.php
+++ Database/WeatherAPI.php
@@ -8,7 +8,10 @@ This query will get us all of VT
 http://api.wunderground.com/api/a8be19e4c204a4b0/history_20100101/q/VT.json
 */
 
+include_once('mySQLDB.php');
+
 class weatherAPIInterface{
+	private $db = new mySQLDB();
 	private $key = 'a8be19e4c204a4b0';
 	private $state = 'VT';
 	private $cities = null;
@@ -47,20 +50,23 @@ class weatherAPIInterface{
 		$weather = array();
 		if(is_null($this->cities)){return;}
 		foreach ($this->cities as $city) {
-			$weatherQuery = "2012-01-01"; //Will be date to query on later on
+			$weather['date'] = "2012-01-01"; //Will be date to query on later on
 			$data = json_decode($this->curlOn('http://api.wunderground.com/api/'.$this->key.'/history_20100101/q/'.$this->state.'/'.$city.'.json'));
 			$dayData = $data->history->dailysummary; $dayData = $dayData[0];
 			//Get the important things out of the query:
-			$weatherQuery.= " " .strval($dayData->maxhumidity);
-			$weatherQuery.= " " .strval($dayData->minhumidity);
-			$weatherQuery.= " " .strval($dayData->rain);
-			$weatherQuery.= " " .strval($dayData->snow);
-			$weatherQuery.= " " .strval($dayData->hail);
-			$weatherQuery.= " " .strval($dayData->precipi);
-			$weatherQuery.= " " .strval($dayData->maxtempi);
-			$weatherQuery.= " " .strval($dayData->mintempi);
-			$weatherQuery.= " " .strval($dayData->meantempi);
-			echo $weatherQuery;
+			$weather['maxHum'] = $dayData->maxhumidity;
+			$weather['minHum'] = $dayData->minhumidity;
+			$weather['rain']   = $dayData->rain;
+			$weather['snow']   = $dayData->snow;
+			$weather['hail']   = $dayData->hail;
+			$weather['precip'] = $dayData->precipi;
+			$weather['maxtemp']= $dayData->maxtempi;
+			$weather['minTemp']= $dayData->mintempi;
+			$weather['avgTemp']= $dayData->meantempi;
+			
+			var_dump($weather);
+
+			$this->db->inputWeather($weather);
 			echo '<br /><br />';
 		}
 	}
--- Database/mySQLDB.php
+++ Database/mySQLDB.php
@@ -3,7 +3,34 @@
 //Database access stuff going to have to put semi sensistive info in, so we'll include that a conf file
 require_once('../../hackConf.php');
 
-var_dump(DATABASE_NAME);
+class mySQLDB{
+	private $con = null;
+
+	public function __construct(){
+		$this->con =  new PDO('mysql:host='.DATABASE_HOST.';dbname='.DATABASE_NAME,DATABASE_USER,DATABASE_PASS);
+		$this->con->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+		$this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+
+	public function inputWeather($wArray){
+		if(!is_null($this->con)){return;}
+
+		//Run the query
+		$result = $this->link->prepare('INSERT INTO weather ("date", "maxHum","minHum","rain","snow","hail","precip","maxTemp","minTemp","avgTemp") VALUES (?, ?, ?, ?,?,?,?,?,?,?);');
+		$result->bindValue(1,$wArray['date'],PDO::PARAM_STR);
+		$result->bindValue(2,$wArray['maxHum'],PDO::PARAM_STR);
+		$result->bindValue(3,$wArray['minHum'],PDO::PARAM_STR);
+		$result->bindValue(4,$wArray['rain'],PDO::PARAM_INT);
+		$result->bindValue(5,$wArray['snow'],PDO::PARAM_INT);
+		$result->bindValue(6,$wArray['hail'],PDO::PARAM_INT);
+		$result->bindValue(7,$wArray['precip'],PDO::PARAM_STR);
+		$result->bindValue(8,$wArray['maxTemp'],PDO::PARAM_STR);
+		$result->bindValue(9,$wArray['minTemp'],PDO::PARAM_STR);
+		$result->bindValue(10,$wArray['avgTemp'],PDO::PARAM_STR);
+		$result->execute();
+
+	}
+}
 
 
 ?>
