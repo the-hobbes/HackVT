886d3cca95a3afc751b302158468825f65de1c59
the-hobbes <phelan.vendeville@gmail.com>
2012-10-20 04:07:07 -0400

    added php script to connect to database

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
+<?php
+	include("../../hackVTconf.php");
+	//connect to server
+	$link = mysql_connect($database, $username, $password);
+	if (!$link) {
+	    die('Not connected : ' . mysql_error());
+	}
+
+	//connect to database
+	$db_selected = mysql_select_db('PVENDEVI_HackVT', $link);
+	if (!$db_selected) {
+	    die ('Can\'t use foo : ' . mysql_error());
+	}
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
